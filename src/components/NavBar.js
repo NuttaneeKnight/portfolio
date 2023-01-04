f102cfee864fb9c022ab90328b4f2766a9c5de4c
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-01-03 22:50:47 -0500

    deleted unused components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
-// import { HashLink } from 'react-router-hash-link';
-// import {BrowserRouter as Router} from "react-router-dom";
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
 export const NavBar = () => {
 
@@ -31,12 +31,9 @@ export const NavBar = () => {
   }
 
   return (
-    // <Router>
+    <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
@@ -48,18 +45,15 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/nuttaneeknight/"><img src={navIcon1} alt="linkedin" /></a>
+                <a href="https://github.com/NuttaneeKnight"><img src={navIcon2} alt="github" /></a>
+                <a href="https://www.instagram.com/nuttanees/"><img src={navIcon3} alt="instagram" /></a>
               </div>
-              {/* <HashLink to='#connect'> */}
-                <button className="vvd"><span>Let’s Connect</span></button>
-              {/* </HashLink> */}
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    // </Router>
+    </Router>
   )
 }
 
---
+++ src/components/Projects.js
@@ -0,0 +1,81 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "We Love Movies",
+      description: 'https://welovemovies-fnt.onrender.com',
+      imgUrl: projImg1,
+    },
+    {
+      title: "Restaurant Reservation App",
+      description: "https://restaurantreservationapp-frontend.onrender.com",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Study Tool App",
+      description: "https://github.com/NuttaneeKnight/Study-Tool-Application",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p></p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Completed</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">In Progress</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Future</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index} 
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>To be Continue...</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Any suggestion on which project I should work on? <br></br>Let me know below, would love to collaborate with fellow coders!</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
