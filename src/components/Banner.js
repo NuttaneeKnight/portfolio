387030f514a4566407c8ce74293733ad4d6d6e5e
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-01-05 18:18:15 -0500

    shorten the animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() *100)
   const [index, setIndex] = useState(1);
-  const period = 2000;
+  const period = 500;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -21,7 +21,7 @@ export const Banner = () => {
     return () => { clearInterval(ticker) };
   }, [text])
 
-  const tick = () => {
+  const tick = () => { 
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
