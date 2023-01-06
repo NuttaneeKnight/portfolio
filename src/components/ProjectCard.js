121b8d39327968e66c8617ec177095ab7bcef550
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-01-05 20:02:42 -0500

    larger link

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,10 +4,10 @@ export const ProjectCard = ({ title, description, imgUrl, }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt='' />
+        <img src={imgUrl} alt=''/>
         <div className="proj-txtx">
           <h5>{title}</h5>
-         <a href={description}><span >{description}</span></a>
+         <a href={description}><span><h4>{description}</h4></span></a>
         </div>
       </div>
     </Col>
