797bdd6ca11d2289328019d9e3ec3c51cbe164bd
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-01-05 19:39:46 -0500

    added profile picture

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import logo from '../assets/img/logo.png'
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={6} sm={3}>
+          <Col size={8} sm={3}>
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={12} className="text-center text-sm-end">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import logo from '../assets/img/logo.png'
 import {
   BrowserRouter as Router
 } from "react-router-dom";
@@ -34,6 +35,9 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
+        <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
